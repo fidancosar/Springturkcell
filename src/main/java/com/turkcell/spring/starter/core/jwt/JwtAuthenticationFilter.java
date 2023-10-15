b3ba9b33937dd92f1852ffd96e03a6d2be0cf61d
fidancosar <141564627+fidancosar@users.noreply.github.com>
2023-10-15 07:19:25 +0300

    Spring-Securty giriş

---
+++ src/main/java/com/turkcell/spring/starter/core/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,31 @@
+package com.turkcell.spring.starter.core.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+
+            filterChain.doFilter(request,response);
+
+            return;
+        }
+        final String jwt = authHeader.substring(7);
+
+    }
+}
